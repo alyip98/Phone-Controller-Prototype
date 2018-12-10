4790c188bf3da7f6b0f3f94ff245089c952c6508
alyip98 <alyip98@gmail.com>
2018-12-10 16:57:43 +0800

    Joystick works nicely
    Working on buttons now

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var players = [];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+	players.push(socket);
   socket.on('joystick', function(msg){
     // io.emit('chat message', msg);
 	console.log("joystick: " + msg);
