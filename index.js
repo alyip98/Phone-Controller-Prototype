2790a53693908562ca2aec0b07d8732003de19d1
alyip98 <alyip98@gmail.com>
2018-12-11 15:50:39 +0800

    Game skeleton complete

--- index.js
+++ index.js
@@ -2,26 +2,60 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-var players = [];
+var gameSocket = null;
+var players = {};
+/*
+ * players id:socketId
+ */
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/game', function(req, res){
+	res.sendFile(__dirname + '/game.html');
 });
 
 io.on('connection', function(socket){
-	players.push(socket);
-  socket.on('joystick', function(msg){
-    // io.emit('chat message', msg);
-	console.log("joystick: " + msg);
-  });
-  
-  socket.on('button', function(msg){
-    // io.emit('chat message', msg);
-	console.log("button: " + msg);
-  });
-  // console.log("user connected");
+	socket.on('joystick', function(msg){
+		console.log("joystick: " + msg);
+		
+		if (gameSocket) {
+			gameSocket.emit('joystick', msg);
+		}
+	});
+
+	socket.on('button', function(msg){
+		// io.emit('chat message', msg);
+		console.log("button: " + msg);
+	});
+
+	socket.on('gameInit', function(msg) {
+		console.log("game session opened");
+		gameSocket = socket;
+		for (var i in Object.keys(players)) {
+			gameSocket.emit("playerJoin", Object.keys(players)[i]);
+		}
+	});
+
+	socket.on('clientNew', function(msg) {
+		players[msg] = socket.id;
+		console.log("player joining: " + msg);
+		if (gameSocket) {
+			gameSocket.emit("playerJoin", msg);
+		}
+	});
+
+	socket.on('clientRejoin', function(msg) {
+		players[msg] = socket.id;
+		console.log("player joining: " + msg);
+		if (gameSocket) {
+			gameSocket.emit("playerJoin", msg);
+		}
+	});
+	// console.log("user connected");
 });
 
 http.listen(port, function(){
-  console.log('listening on *:' + port);
+	console.log('listening on *:' + port);
 });
